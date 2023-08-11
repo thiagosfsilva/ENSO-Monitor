cde53875e2dea149b54375c51e3d0d352b35a3fb
Thiago Silva <thiago.sf.silva@stir.ac.uk>
2023-08-11 14:46:25 +0100

    layout updated using bootstrap

--- App.py
+++ App.py
@@ -1,12 +1,14 @@
 from dash import Dash, html, dcc
 import pandas as pd
 import plotly.graph_objects as go
-import get_telem
+import dash_bootstrap_components as dbc
 
 ###### Get data
 #curData = pd.read_pickle('curData.pkl').reset_index()
 curData = pd.read_pickle('data/curData.pkl').reset_index()
 hisData = pd.read_pickle('data/hisData.pkl').reset_index()
+upDate = pd.read_pickle('data/upDate.pkl').reset_index()
+updated = str(upDate._get_value(0,0))
 
 # Calculate statistics
 doyMean = hisData.groupby('Doy').Nivel.agg(['mean']).reset_index()
@@ -22,27 +24,27 @@ doy2022 = hisData[hisData['Yr']==2022]
 # Generate Plot
 fig_level = go.Figure([
     # Current data
-    go.Scatter(name='2023',x=curData['Doy'],y=curData['Nivel'],mode='lines',line=dict(color='#2c7bb6'),),
+    go.Scatter(name='2023',x=curData['Doy'],y=curData['Nivel'],mode='lines',line=dict(color='#2c7bb6', width=4, smoothing=1),line_shape='spline'),
     # Historical droughts
-    go.Scatter(name='2022',x=doy2022['Doy'],y=doy2022['Nivel'],mode='lines',line=dict(color='#1a9641'),),
-    go.Scatter(name='2015',x=doy2015['Doy'],y=doy2015['Nivel'],mode='lines',line=dict(color='#ffffb2'),),
-    go.Scatter(name='2010',x=doy2010['Doy'],y=doy2010['Nivel'],mode='lines',line=dict(color='#e31a1c'),),
-    go.Scatter(name='2005',x=doy2005['Doy'],y=doy2005['Nivel'],mode='lines',line=dict(color='#fecc5c'),),
-    go.Scatter(name='1998',x=doy1998['Doy'],y=doy1998['Nivel'],mode='lines',line=dict(color='#fd8d3c'),),
+    go.Scatter(name='2022',x=doy2022['Doy'],y=doy2022['Nivel'],mode='lines',line=dict(color='#1a9641',width=1, smoothing=1),line_shape='spline'),
+    go.Scatter(name='2015',x=doy2015['Doy'],y=doy2015['Nivel'],mode='lines',line=dict(color='#fecc5c',width=1, smoothing=1),line_shape='spline'),
+    go.Scatter(name='2010',x=doy2010['Doy'],y=doy2010['Nivel'],mode='lines',line=dict(color='#800026',width=1, smoothing=1),line_shape='spline'),
+    go.Scatter(name='2005',x=doy2005['Doy'],y=doy2005['Nivel'],mode='lines',line=dict(color='#fd8d3c',width=1, smoothing=1),line_shape='spline'),
+    go.Scatter(name='1998',x=doy1998['Doy'],y=doy1998['Nivel'],mode='lines',line=dict(color='#e31a1c',width=1, smoothing=1),line_shape='spline'),
     # Climatology
-    go.Scatter(name='Mean 1979-2022',x=doyMean['Doy'],y=doyMean['mean'],mode='lines',line=dict(color='rgb(0, 0, 0)', dash='dash'),),
+    go.Scatter(name='Mean 1979-2022',x=doyMean['Doy'],y=doyMean['mean'],mode='lines',line=dict(color='rgb(100, 100, 100)', dash='dash'),line_shape='spline'),
     go.Scatter(name='95% CI',x=doySD['Doy'],y=doyMean['mean'] + 1.96 * doySD['std'],mode='lines',marker=dict(color="#444"),
-               line=dict(width=0),showlegend=False),
-    go.Scatter(name='95% CI',x=doySD['Doy'],y=doyMean['mean'] - 1.96 * doySD['std'],marker=dict(color="#444"),line=dict(width=0),
-               mode='lines',fillcolor='rgba(100, 100, 100, 0.2)',fill='tonexty',)
+               line=dict(width=0, smoothing=0.5),showlegend=False,line_shape='spline'),
+    go.Scatter(name='95% CI',x=doySD['Doy'],y=doyMean['mean'] - 1.96 * doySD['std'],marker=dict(color="#444"),line=dict(width=0, smoothing=0.5),
+               mode='lines',fillcolor='rgba(100, 100, 100, 0.2)',fill='tonexty',line_shape='spline')
 ])
 
 fig_level.update_layout(
     yaxis_title='Water level (cm)',
     xaxis_title='Day of the year',    
     #title='Current and historical water leveles, Fonte Boa',
-    width=1400,
-    height=800,
+    #width=1400,
+    #height=800,
     margin=dict(l=50,r=50,b=100,t=30,pad=0),
     hovermode='x unified'
 )
@@ -53,14 +55,34 @@ fig_level.update_layout(spikedistance=1000, hoverdistance=100)
 
 # Dash app
 
-app = Dash(__name__)
+app = Dash(__name__, external_stylesheets=[dbc.themes.FLATLY],
+           meta_tags=[
+        {"name": "viewport", "content": "width=device-width, initial-scale=1"},
+    ],)
 server = app.server
 
-app.layout = html.Div([
-    html.H1(children='ENSO 2003 drought monitor', style={'textAlign':'center'}),
-    #dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection'),
-    dcc.Graph(id='fig-level', figure=fig_level)
-])
+app.layout = dbc.Container([
+    dbc.NavbarSimple(
+    children=[
+        dbc.NavItem(dbc.NavLink(f'Last updated on {updated}', href="#")),
+    ],
+    brand="ENSO 2003 drought monitor",
+    brand_href="#",
+    color="primary",
+    dark=True,
+    ),
+    #dbc.Row([
+    #    dbc.Col([           
+    #html.H1(children='ENSO 2003 drought monitor', style={'textAlign':'center'}),
+    #html.H2(children=f'Last updated on {updated}', style={'textAlign':'center'}),
+    #html.Hr(),
+    #        ])
+    #    ]),
+    dbc.Row(
+    dcc.Graph(id='fig-level', figure=fig_level,  style={'aspect-ratio': '21 / 10'}),
+    )
+],
+fluid=True)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- get_telem.py
+++ get_telem.py
@@ -1,5 +1,6 @@
 import requests
 import pandas as pd
+from datetime import datetime
 
 def get_telem(export=False):
     # Set request parameters
@@ -11,6 +12,7 @@ def get_telem(export=False):
 
     # Make request
     response = requests.get(ana_url, params = params)
+    upDate = pd.DataFrame([datetime.today().date()])
 
     # Parse response as dataframe and clean it
     rawTable = pd.read_xml(response.content, namespaces={'msdata':"urn:schemas-microsoft-com:xml-msdata"}, xpath='.//DadosHidrometereologicos')
@@ -31,9 +33,10 @@ def get_telem(export=False):
 
     # Export to file
     if export:
-        curData.to_pickle('curData.pkl')
+        curData.to_pickle('data/curData.pkl')
+        upDate.to_pickle('data/upDate.pkl')
     
     return curData
     
 if __name__ == "__main__":
-    get_telem()
+    get_telem(export=True)
